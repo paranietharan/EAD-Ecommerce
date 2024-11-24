bbf8937c8709e4cdb3d0e96df8ff873255a86de8
paranietharan <paranie20@gmail.com>
2024-11-24 21:37:40 +0530

    final change

--- services/product/src/main/java/com/teamz/product/product/ProductService.java
+++ services/product/src/main/java/com/teamz/product/product/ProductService.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
