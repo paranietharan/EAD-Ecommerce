737ac64dbc9810fc9e132e662092f9f79aaec522
Kirushanth <kirushan06@gmail.com>
2024-11-27 17:54:09 +0530

    CORS Solved

--- services/customer/src/main/java/com/teamz/customer/controller/AuthController.java
+++ services/customer/src/main/java/com/teamz/customer/controller/AuthController.java
@@ -13,9 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth/")
-@CrossOrigin
+@CrossOrigin("*")
 public class AuthController {
-
     private final AuthService authService;
     private final RefreshTokenService refreshTokenService;
     private final JwtService jwtService;
